9fcd090b659ea853d0aab13e9767595c32d2b20f
Asraf Ansari <50796435+asraf3101aa@users.noreply.github.com>
2025-08-09 21:25:15 +0545

    feat: user registration (#5)

--- account/urls.py
+++ account/urls.py
@@ -1,7 +1,7 @@
 from django.urls import  path, include
-from account.views import   LoginView, UserViewSet
 from rest_framework_simplejwt.views import TokenRefreshView
 from rest_framework.routers import DefaultRouter
+from account.views import LoginView, UserViewSet
 
 router = DefaultRouter()
 router.register(r'users', UserViewSet, basename='user')
